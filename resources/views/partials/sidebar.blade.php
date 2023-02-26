e7deeb00f951f5ce9e3a92bb689c4243462f8604
mass-andra05 <deandratisna@gmail.com>
2023-02-26 13:55:19 +0700

    export excell semua master data

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -12,7 +12,7 @@
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link text-white {{ (Request::segment(1) == '') ? "active bg-gradient-success" : "" }}"
-                    href="/">
+                    href="/humas/dashboard">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="fa fa-home me-sm-1"></i>
                     </div>
